b6d4ce3047adabfaaccacd4abc0037882e602d88
Angela <antunezangela89@gmail.com>
2023-11-15 00:04:42 +0100

    Html, css and js for tracking scroll, background color for extra menu, some users cards, delete alt from buttons, improve footer icons

---
+++ public/js/header.js
@@ -0,0 +1,33 @@
+const observer = new IntersectionObserver(trackingScroll, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.25
+});
+
+function trackingScroll (entries) {
+    const element = entries.filter(a => a.isIntersecting).sort((a, b) => b.intersectionRatio - a.intersectionRatio)?.[0]?.target;
+
+    if (!element) {
+        return;
+    }
+
+    document.querySelectorAll('#header__navbar a.active').forEach(a => {
+        a.classList.remove('active');
+    })
+
+    document.querySelector(`#header__navbar a[href="#${element.id}"]`).classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = [
+        'description',
+        'features',
+        'screens',
+    ]
+
+    for (const id of sections) {
+        const element = document.getElementById(id);
+
+        observer.observe(element);
+    }
+})
